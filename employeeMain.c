e37896bdfb4333f171e25f577c0338322a166cfb
aj-cpp <ajujun@cpp.edu>
2022-10-21 22:09:59 -0700

    Create tests for search by phone and salary

--- employeeMain.c
+++ employeeMain.c
@@ -18,6 +18,14 @@ int main(void)
                                        int sizeTable, 
                                        char *nameToFind);
 
+  PtrToEmployee searchEmployeeByPhone(const Employee table[],
+                                      int table_size,
+                                      char *phone_number);
+
+  PtrToEmployee searchEmployeeBySalary(const Employee table[],
+                                       int table_size,
+                                       double salary);
+
   // defined in employeeTable.c
   extern Employee EmployeeTable[];
   extern const int EmployeeTableEntries;
@@ -32,10 +40,50 @@ int main(void)
     printf("Employee ID is NOT found in the record\n");
 
   // Example found
+  matchPtr = searchEmployeeByName(EmployeeTable, EmployeeTableEntries, "Tony Bobcat");
   if (matchPtr != NULL)
     printf("Employee Tony Bobcat is in record %d\n", matchPtr - EmployeeTable);
   else
     printf("Employee Tony Bobcat is NOT found in the record\n");
 
-    return EXIT_SUCCESS;
+  // TEST: searchEmployeeByPhone
+  // Not found
+  matchPtr = searchEmployeeByPhone(EmployeeTable, EmployeeTableEntries, "123-123-1234");
+  if (matchPtr != NULL)
+  {
+    printf("ERROR! Expected: NULL, but found a record. Line: %d\n", __LINE__);
+    return EXIT_FAILURE;
+  }
+  printf("Searching: 123-123-1234 -> Employee not found.\n");
+
+  // Found
+  matchPtr = searchEmployeeByPhone(EmployeeTable, EmployeeTableEntries, "714-555-2749");
+  if (matchPtr == NULL)
+  {
+    printf("ERROR! Expected search 714-555-2749, but found NULL. Line: %d\n", __LINE__);
+    return EXIT_FAILURE;
+  }
+  printf("Searching: 714-555-2749 -> Found in record %d\n", matchPtr - EmployeeTable);
+
+  // TEST: searchEmployeeBySalary
+  // Not found
+  matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 1.23);
+  if (matchPtr != NULL)
+  {
+    printf("ERROR! Expected: NULL, but found a record. Line: %d\n", __LINE__);
+    return EXIT_FAILURE;
+  }
+  printf("Searching: 0 -> Employee not found.\n");
+
+  // Found
+  matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 8.78);
+  if (matchPtr == NULL)
+  {
+    printf("ERROR! Expected search 8.78, but found NULL. Line: %d\n", __LINE__);
+    return EXIT_FAILURE;
+  }
+  printf("Searching: 8.78 -> Found in record %d\n", matchPtr - EmployeeTable);
+
+
+  return EXIT_SUCCESS;
 }
